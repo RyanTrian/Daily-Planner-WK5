7b4f3a606fb8ca8bc600269fb9bcc6adc80827a0
Minh Truong <mptruong49@gmail.com>
2022-10-02 11:02:58 -0600

    indicate present past and future, render timeblock on the left, correct some styling

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,18 +3,41 @@
 $(function() {
     $('#currentDay').text(moment().format("dddd, MMMM Do"));
     for (let i = 9; i < 18; i++) {
-        let row = $(`<section class='row' id='row${i}'></section>`);
+        let row = $(`<section class='row' id='row${i}' data-time=${i}></section>`);
 
-        let timeblock = $('<div class="col-sm-2"><p class="hour">' + '</p>');
+        let timeblock = $('<div class="col-sm-2" id="time-block"><p class="hour">' +renderTimeBlock(i) + '</p>');
 
-        let isTask = $(`<div class="col-sm-8 past"><textarea id='text${i}' class="description" placeholder="Add your event here..."></textarea>`);
+        let isTask = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);
 
-        let saveBtn = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`);
-
-
+        let saveBtn = $(`<div class="col-sm-2" id="save-div"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`);
+        
         row.append(timeblock);
         row.append(isTask);
         row.append(saveBtn);
+
         $('.container').append(row);
     }
-})
+    // render the time + am or pm on the left side to indicate time block
+    function renderTimeBlock(hour) {
+        let time = hour < 12 ? hour + 'am' :
+                 hour === 12 ? hour + 'pm' :
+                 hour - 12 + 'pm'
+        return time;
+    }
+    // Check for past present and future timeblock then give them the corresponded class
+    function whatColor() {
+        let currentHour = moment().hours();
+        for (let i = 9; i < 18; i++) {
+            if ($(`#row${i}`).data('time') == currentHour) {
+                $(`#text${i}`).addClass("present");
+            } else if (currentHour < $(`#row${i}`).data('time')) {
+                $(`#text${i}`).addClass("future");
+            }
+        }
+    }
+    
+    setInterval(function () {
+      whatColor();
+    }, 1000);
+    
+});
